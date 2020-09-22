1ec2e9ba33dd942014d08673150fb9fc6c62e2f3
NargisUsman <nargisusman4@gmail.com>
2020-09-22 17:23:19 -0400

    Second Homework

--- src/Class2/MyJavaClass2.java
+++ src/Class2/MyJavaClass2.java
@@ -7,24 +7,23 @@ public class MyJavaClass2 {
         double cTemp = (fTemp - 32) * 5/9;
         System.out.println(fTemp + " F is equal to " + cTemp + " C"); // F -> C
 
-        double fTemp2 = 10;
-        double kTemp = (fTemp2 + 459.67) * 5/9;
-        System.out.println(fTemp2 + " F is equal to " + kTemp + " K"); // F -> K
+        double kTemp = (fTemp + 459.67) * 5/9;
+        System.out.println(fTemp + " F is equal to " + kTemp + " K"); // F -> K
 
-        double kTemp2 =10;
-        double cTemp2 =kTemp2 - 273.15;
-        System.out.println(kTemp2 + " K is equal to " + cTemp2 + " C"); // K -> C
+        kTemp =10;
+        cTemp =kTemp - 273.15;
+        System.out.println(kTemp + " K is equal to " + cTemp + " C"); // K -> C
 
-        double kTemp3 = 10;
-        double fTemp3 = kTemp3 * 9/5 - 459.67;
-        System.out.println(kTemp3 + " K is equal to " + fTemp3 + " F"); // K -> F
+        kTemp = 10;
+        fTemp = kTemp * 9/5 - 459.67;
+        System.out.println(kTemp + " K is equal to " + fTemp + " F"); // K -> F
 
-        double cTemp3 =10;
-        double fTemp4 = cTemp3 * 9/5 + 32;
-        System.out.println(cTemp3 + " C is equal to " + fTemp4 + " F"); // C -> F
+        cTemp =10;
+        fTemp = cTemp * 9/5 + 32;
+        System.out.println(cTemp + " C is equal to " + fTemp + " F"); // C -> F
 
-        double cTemp4 = 10;
-        double kTemp4 =cTemp4 + 273.15;
-        System.out.println(cTemp4 + " C is equal to " + kTemp4 + " K"); // C -> K
+        cTemp = 10;
+        kTemp =cTemp + 273.15;
+        System.out.println(cTemp + " C is equal to " + kTemp + " K"); // C -> K
     }
 }
